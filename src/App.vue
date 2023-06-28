4cf9b6d0e539109cd200c45fd436991b15518a78
米川貴也 <zuomujianwu1@gmail.com>
2023-06-28 21:02:20 +0900

    v-model

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-// import { computed } from "vue";
+import {ref, computed } from "vue";
 
 // const answer = computed(() => {
 //   return 1 + 1;
@@ -7,9 +7,11 @@
 
 const message = 'ようこそ　<strong>ヨネ</strong>さん'
 const welecomeColor = 'blue'
+const myname = ref('')
 </script>
 
 <template>
   <h1>Vue 3</h1>
-  <p v-html="message" :style="{backgroundColor:welecomeColor, 'color': 'white'}" ></p>
+  <p>お名前は？：<input type="text" size="10" v-model="myname"></p>
+  <p v-html="myname" :style="{backgroundColor:welecomeColor, 'color': 'white'}" ></p>
 </template>
