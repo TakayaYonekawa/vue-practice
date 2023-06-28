125ea3126a79221d69b869b8a5e7e5542c5beca8
米川貴也 <zuomujianwu1@gmail.com>
2023-06-28 20:59:20 +0900

    v-bind

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <script setup>
-import { computed } from "vue";
+// import { computed } from "vue";
 
-const answer = computed(() => {
-  return 1 + 1;
-});
+// const answer = computed(() => {
+//   return 1 + 1;
+// });
+
+const message = 'ようこそ　<strong>ヨネ</strong>さん'
+const welecomeColor = 'blue'
 </script>
 
 <template>
   <h1>Vue 3</h1>
-  <p>1 + 1 = {{ answer }}</p>
+  <p v-html="message" :style="{backgroundColor:welecomeColor, 'color': 'white'}" ></p>
 </template>
