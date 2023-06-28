b46536189def967e6187f44d9a01f8d18888fc45
米川貴也 <zuomujianwu1@gmail.com>
2023-06-28 22:01:32 +0900

    @click

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,13 @@
 import { ref } from "vue";
 
   const number = ref(0)
+  const add = () => {
+    number.value++
+  }
 </script>
 
 
 <template>
   <input type="text" size="30" v-model="number">
-  <button>+1</button>
+  <button @click="add()">+1</button>
 </template>
