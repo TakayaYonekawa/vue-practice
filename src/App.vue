8c9b0e33cb43df597305285187f774174f16889a
米川貴也 <zuomujianwu1@gmail.com>
2023-06-28 22:05:15 +0900

    newTodo

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
 <script setup>
 import { ref } from "vue";
 
-  const number = ref(0)
-  const add = () => {
-    number.value++
-  }
+  const newTodo = ref('')
 </script>
 
 
 <template>
-  <input type="text" size="30" v-model="number">
-  <button @click="add()">+1</button>
+  <input type="text" size="30" v-model="newTodo">
+  <button >追加</button>
 </template>
