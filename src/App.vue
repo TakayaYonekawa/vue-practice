5197a927dbe1ca64f3a2e62c66ee097832b9abe5
米川貴也 <zuomujianwu1@gmail.com>
2023-06-30 20:20:12 +0900

    　TODOを追加してください

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,8 @@ import { ref } from "vue";
   <input type="text" size="30" v-model="newTodo">
   <button @click="addTodo()">追加</button>
 
-  <ul v-show="todos.length > 0">
+  <ul v-if="todos.length > 0">
     <li v-for="(todo, i) in todos" :key="i"> {{todo}} <span @click="removeTodo(i)" style="cursor: pointer;">x</span> </li>
   </ul>
+  <p v-else>*　TODOを追加してください</p>
 </template>
