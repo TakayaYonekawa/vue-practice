be574741022252ba7a34d73e3caaf1d7039382a9
米川貴也 <zuomujianwu1@gmail.com>
2023-06-30 22:26:33 +0900

    removeTodo

---
+++ src/components/TodoList.vue
@@ -0,0 +1,16 @@
+<script setup>
+    defineProps(['todos'])
+    const emit = defineEmits(['removeTodo'])
+
+    const clickRemove = (i) => emit('removeTodo', i)
+</script>
+
+
+<template>
+    <ul v-if="todos.length > 0">
+        <li v-for="(todo, i) in todos" :key="i"> {{todo}} <span @click="clickRemove(i)" style="cursor: pointer;">x</span> </li>
+      </ul>
+      <p v-else>*　TODOを追加してください</p>
+    
+</template>
+
